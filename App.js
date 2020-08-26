9a11a39e8c802ce76e6f25a58c3926960780b362
Max <maxpower@riseup.net>
2020-08-25 23:58:13 -0300

    Pendiente de geolocalización

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { WebView } from 'react-native-webview';
 
+//TODO, activar la funcoinalidad de la geolocalización.
 export default class App extends Component<Props> {
- 
   render() {
     return (
       <WebView
